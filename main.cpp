2e1a241e4b91010b4eed64c3ccfcfed89d08e8fd
zhuoxiao yang <91831716+yangtianwang@users.noreply.github.com>
2021-11-15 12:28:53 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int n,a=1,b=1,c;
+   cin>>n;
+   if(n<2){
+      cout<<"errore"<<endl;
+   }else{
+      cout<<a<<"\n"<<b<<endl;
+      for(int i-2;i<n;i++){
+         c=a+b;
+         a=b;
+         b=c;
+         cout<<c<<endl;
+      }
+   }
    return 0;
 }
